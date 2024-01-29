f66d1f43e9f60b84e8f5c611b2e836d3c373f588
vaibhav19121 <Vaibhav.M@ecinfosolutions.com>
2024-01-29 23:57:01 +0530

    project setup

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import Nav from './src/Navigation';
+import {Provider} from 'react-redux';
+import {persistStore} from 'redux-persist';
 
+import {PersistGate} from 'redux-persist/integration/react';
+import Toast, {BaseToast} from 'react-native-toast-message';
 
+import {navigationRef} from './src/Utils/navigationService';
+import store from './src/Store';
 
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
+  var persistor = persistStore(store);
+  const toastConfig = {
+    success: ({...rest}) => (
+      <BaseToast
+        {...rest}
+        style={{borderLeftColor: '#90EE90'}}
+        text1Style={{
+          fontFamily: 'Poppins-Regular',
+          fontWeight: '500',
+        }}
+      />
+    ),
+    error: ({...rest}) => (
+      <BaseToast
+        {...rest}
+        style={{borderLeftColor: 'crimson'}}
+        text1Style={{
+          fontFamily: 'Poppins-Regular',
+          fontWeight: '500',
+        }}
+      />
+    ),
+  };
 
   return (
-    <SafeAreaView >
-      <Text>
-        Hello WOrld
-      </Text>
-    </SafeAreaView>
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef}>
+        <PersistGate persistor={persistor}>
+          <Nav />
+          <Toast config={toastConfig} />
+        </PersistGate>
+      </NavigationContainer>
+    </Provider>
   );
-}
-
+};
 
 export default App;
---
+++ src/Screens/Home.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+export default function Home() {
+  return (
+    <View style={{flex: 1, backgroundColor: 'red'}}>
+      <Text style={{color: 'white'}}>Home</Text>
+    </View>
+  );
+}
