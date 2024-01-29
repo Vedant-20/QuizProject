36aab5aa43960eccd12a41a862376d0f00933cf9
vaibhav19121 <Vaibhav.M@ecinfosolutions.com>
2024-01-29 23:57:17 +0530

    ..

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,6 +1,5 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function Home() {
   return (
